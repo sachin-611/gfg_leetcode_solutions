d82cc6bfb87c1c84b19144c709e704ca4a8c32a0
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2022-08-24 23:55:08 +0530

    Time: 23 ms (40.00%), Space: 6 MB (66.08%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -2,55 +2,43 @@ class Solution {
 public:
     int romanToInt(string s) {
         int ans=0;
-        for(int i=0;i<s.length();)
+        for(int i=0;i<s.length();i++)
         {
             if(s[i]=='I')
             {
                 if(i+1<s.length() and s[i+1]=='X')
-                    ans+=9,i+=2;
+                    ans+=9,++i;
                 else if(i+1<s.length() and s[i+1]=='V')
-                    ans+=4,i+=2;
+                    ans+=4,++i;
                 else 
-                    ans+=1,i+=1;
+                    ans+=1;
             }
             else if(s[i]=='X')
             {
                 if(i+1<s.length() and s[i+1]=='L')
-                    ans+=40,i+=2;
+                    ans+=40,++i;
                 else if(i+1<s.length() and s[i+1]=='C')
-                    ans+=90,i+=2;
+                    ans+=90,++i;
                 else 
-                    ans+=10,i+=1;   
+                    ans+=10;   
             }
             else if(s[i]=='C')
             {
                 if(i+1<s.length() and s[i+1]=='D')
-                    ans+=400,i+=2;
+                    ans+=400,++i;
                 else if(i+1<s.length() and s[i+1]=='M')
-                    ans+=900,i+=2;
+                    ans+=900,++i;
                 else 
-                    ans+=100,i+=1;
+                    ans+=100;
             }
             else if(s[i]=='V')
-            {
-                i+=1;
                 ans+=5;
-            }
             else if(s[i]=='L')
-            {
-                i+=1;
                 ans+=50;
-            }
             else if(s[i]=='D')
-            {
-                i+=1;
                 ans+=500;
-            }
             else if(s[i]=='M')
-            {
-                i+=1;
                 ans+=1000;
-            }
         }
         return ans;
     }
