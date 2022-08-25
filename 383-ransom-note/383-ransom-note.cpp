bc72ab4029b433724a06c10bf37aec834d556687
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2022-08-25 12:37:58 +0530

    Time: 15 ms (84.13%), Space: 8.8 MB (72.69%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
         int arr[26]={};
-        for(char i:magazine)
+        for(char &i:magazine)
             arr[i-'a']++;
-        for(char i:ransomNote)
+        for(char &i:ransomNote)
         {
             if(arr[i-'a']==0)
                 return false;
