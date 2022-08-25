0e544f99c4510b3fa79728ae03efc5f3da8a1e38
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2022-08-25 12:38:49 +0530

    Time: 19 ms (72.14%), Space: 8.1 MB (99.66%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -3,12 +3,12 @@ public:
     bool canConstruct(string &ransomNote, string &magazine) {
         int arr[26]={};
         for(char &i:magazine)
-            arr[i-'a']++;
+            ++arr[i-'a'];
         for(char &i:ransomNote)
         {
             if(arr[i-'a']==0)
                 return false;
-            arr[i-'a']--;
+            --arr[i-'a'];
         }
         return true;
     }
