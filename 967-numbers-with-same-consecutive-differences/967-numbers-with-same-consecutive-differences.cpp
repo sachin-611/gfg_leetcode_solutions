37d9ea2398ace2482885481b03d834c069b98082
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2022-09-03 23:18:54 +0530

    Time: 17 ms (7.45%), Space: 14.6 MB (5.89%) - LeetHub

---
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    unordered_set<int>res;
+    void gen(int n,int k,string s)
+    {
+        if(s.length()==n)
+        {
+            res.insert(stoi(s));
+            return;
+        }
+        // for(int i=0;i<10;i++)
+        // {
+        //     if(s.empty())
+        //     {
+        //         s.push_back('0'+i);
+        //         gen(n,k,s);
+        //         s.pop_back();
+        //     }
+        //     else
+        //     {
+                int c=s[s.length()-1]-'0';
+                if(c+k<=9)
+                {
+                    c=c+k;
+                    s.push_back(c+'0');
+                    gen(n,k,s);
+                    s.pop_back();
+                    c-=k;
+                }
+                if(c-k>=0)
+                {
+                    c-=k;
+                    s.push_back(c+'0');
+                    gen(n,k,s);
+                    s.pop_back();
+                    c+=k;
+                }
+        //     }
+        // }
+    }
+    vector<int> numsSameConsecDiff(int n, int k) {
+        res.clear();
+        string s="1";
+        for(int i=1;i<10;i++,s[0]++)
+            gen(n,k,s);
+        vector<int>re(res.begin(),res.end());
+        return re;
+    }
+};
