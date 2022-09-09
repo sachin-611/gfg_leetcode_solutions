76a63bdfafff8e3d44f6912f64c666de1daced25
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2022-09-09 16:04:40 +0530

    Time: 1672 ms (5.05%), Space: 127.1 MB (30.04%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -4,18 +4,27 @@ public:
         int n=b.size();
         sort(b.begin(),b.end());
         vector<int>pre(n);
-        vector<int>a(n);
-        for(int i=0;i<n;i++)a[i]=b[i][0];
         pre[n-1]=b[n-1][1];
         for(int i=n-2;i>=0;i--)
             pre[i]=max(pre[i+1],b[i][1]);
         int cnt=0;
-        // for(int i:a)
-        //     cout<<i<<" ";
-        // cout<<endl;
         for(int i=0;i<n;i++)
         {
-            int in=upper_bound(a.begin(),a.end(),a[i])-a.begin();
+            int in=-1;
+            int low=0,high=n-1;
+            while(low<=high)
+            {
+                int mid=(high-low)/2+low;
+                if(b[mid][0]<=b[i][0])
+                {
+                    if(b[mid][0]==b[i][0])
+                        in=mid;
+                    low=mid+1;
+                }
+                else
+                    high=mid-1;
+            }
+            in++;
             if(in<n and pre[in]>b[i][1])
                 cnt++;
         }
