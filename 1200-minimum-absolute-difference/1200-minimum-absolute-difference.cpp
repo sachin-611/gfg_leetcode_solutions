cc543219cd558785f7e028378ee6ecd3e7c47454
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2022-09-13 18:33:51 +0530

    Time: 148 ms (20.30%), Space: 32.2 MB (78.18%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
@@ -4,11 +4,11 @@ public:
         int minis=1000000000;
         sort(arr.begin(),arr.end());
         for(int i=1;i<arr.size();i++)
-            minis=min(minis,arr[i]-arr[i-1]);
+        {minis=min(minis,arr[i]-arr[i-1]);}
         vector<vector<int>>a;
         for(int i=1;i<arr.size();i++)
-            if(minis==arr[i]-arr[i-1])
-                a.push_back({arr[i-1],arr[i]});
+        {if(minis==arr[i]-arr[i-1])
+        {a.push_back({arr[i-1],arr[i]});}}
         return a;
     }
 };
