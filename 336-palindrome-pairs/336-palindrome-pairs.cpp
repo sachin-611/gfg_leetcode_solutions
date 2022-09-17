3f3fe878890737bec66badcfc72113e033acead2
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2022-09-17 18:28:56 +0530

    Time: 1864 ms (43.97%), Space: 86.2 MB (81.09%) - LeetHub

--- 336-palindrome-pairs/336-palindrome-pairs.cpp
+++ 336-palindrome-pairs/336-palindrome-pairs.cpp
@@ -15,29 +15,6 @@ public:
     vector<vector<int>> palindromePairs(vector<string>& words) {
         vector<vector<int>>ans;
         int n=words.size();
-//         unordered_map<string, int> m;
-//         for(int i=0;i<n;++i){
-            
-//             auto s= words[i];
-//             reverse(s.begin(),s.end());
-//             m[s]=i;
-//         }
-        
-//         for(int i=0; i<n; ++i){
-//             for(int j=0; j<=words[i].size(); ++j){
-//                 string st1 = words[i].substr(0,j); // prefix
-//                 string st2 = words[i].substr(j);    //sufix           
-                 
-//                  if(m.count(st1) && check(st2) && m[st1] != i) {
-//                      ans.push_back({i, m[st1]});    
-//                  }
-
-//                  if(!st1.empty() && m.count(st2) && check(st1) && m[st2] != i) {
-//                      ans.push_back({m[st2], i});
-//                  }
-//             }            
-//         }
-//         return ans;
         unordered_map<string,int>mp;
         for(int i=0;i<n;i++)
         {
