f53d9861d4acf279136a59410ceaf8f18708f814
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2022-09-17 18:28:38 +0530

    Time: 1891 ms (42.97%), Space: 86.3 MB (81.09%) - LeetHub

--- 336-palindrome-pairs/336-palindrome-pairs.cpp
+++ 336-palindrome-pairs/336-palindrome-pairs.cpp
@@ -15,54 +15,55 @@ public:
     vector<vector<int>> palindromePairs(vector<string>& words) {
         vector<vector<int>>ans;
         int n=words.size();
-        unordered_map<string, int> m;
-        for(int i=0;i<n;++i){
+//         unordered_map<string, int> m;
+//         for(int i=0;i<n;++i){
             
-            auto s= words[i];
-            reverse(s.begin(),s.end());
-            m[s]=i;
-        }
+//             auto s= words[i];
+//             reverse(s.begin(),s.end());
+//             m[s]=i;
+//         }
         
-        for(int i=0; i<n; ++i){
-            for(int j=0; j<=words[i].size(); ++j){
-                string st1 = words[i].substr(0,j); // prefix
-                string st2 = words[i].substr(j);    //sufix           
+//         for(int i=0; i<n; ++i){
+//             for(int j=0; j<=words[i].size(); ++j){
+//                 string st1 = words[i].substr(0,j); // prefix
+//                 string st2 = words[i].substr(j);    //sufix           
                  
-                 if(m.count(st1) && check(st2) && m[st1] != i) {
-                     ans.push_back({i, m[st1]});    
-                 }
+//                  if(m.count(st1) && check(st2) && m[st1] != i) {
+//                      ans.push_back({i, m[st1]});    
+//                  }
 
-                 if(!st1.empty() && m.count(st2) && check(st1) && m[st2] != i) {
-                     ans.push_back({m[st2], i});
-                 }
-            }            
-        }
-        return ans;
-        map<string,vector<int>>mp;
+//                  if(!st1.empty() && m.count(st2) && check(st1) && m[st2] != i) {
+//                      ans.push_back({m[st2], i});
+//                  }
+//             }            
+//         }
+//         return ans;
+        unordered_map<string,int>mp;
         for(int i=0;i<n;i++)
         {
             string temp=words[i];
             reverse(temp.begin(),temp.end());
-            mp[temp].push_back(i);
+            mp[temp]=i;
         }
         for(int i=0;i<n;i++)
         {
             int m=words[i].size();
+            string s1="";
+            string s2=words[i];
             for(int j=0;j<=m;j++)
             {
-                string s1=words[i].substr(0,j);
-                string s2=words[i].substr(j);
-                if(check(s1) and mp.count(s2))
+                if(s1.empty()==false and  mp.count(s2) and i!=mp[s2] and check(s1))
                 {
-                    for(auto kms:mp[s2])
-                        if(i!=kms)
-                            ans.push_back({kms,i});
+                            ans.push_back({mp[s2],i});
                 }
-                if(s1.empty()==false and check(s2) and mp.count(s1))
+                if( mp.count(s1) and i!=mp[s1] and check(s2))
                 {
-                    for(auto kms:mp[s1])
-                        if(i!=kms)
-                            ans.push_back({kms,i});
+                            ans.push_back({i,mp[s1]});
+                }
+                if(j!=m)
+                {
+                    s2.erase(0,1);
+                    s1.push_back(words[i][j]);   
                 }
             }
         }
