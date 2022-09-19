987f7cb7020339516e3a4254704447b75d25a763
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2022-09-19 20:22:46 +0530

    Time: 318 ms (6.49%), Space: 52.7 MB (12.59%) - LeetHub

---
+++ 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<string>> findDuplicate(vector<string>& paths) {
+        map<string,vector<string>>m;
+        for(auto i :paths)
+        {
+            stringstream ss(i);
+            istream_iterator<std::string> begin(ss);
+            istream_iterator<std::string> end;
+            vector<string> tokens(begin, end);
+            for(int j=1;j<tokens.size();j++)
+            {
+                string names=tokens[0];
+                if(names[names.size()-1]!='/')
+                    names.push_back('/');
+                int k=0;
+                string data="";
+                while(k<tokens[j].size() and tokens[j][k]!='(')
+                    names.push_back(tokens[j][k++]);
+                k++;
+                while(k<tokens[j].size() and tokens[j][k]!=')')
+                    data.push_back(tokens[j][k++]);
+                m[data].push_back(names);
+            }
+        }
+        vector<vector<string>>res;
+        for(auto i:m)
+            if(i.second.size()>1)
+                res.push_back(i.second);
+        return res;
+    }
+};
