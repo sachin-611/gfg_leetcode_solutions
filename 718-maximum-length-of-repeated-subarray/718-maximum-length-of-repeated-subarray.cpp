065aa4a53520b94b54406b5d1d560946e7a5bce6
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2022-09-20 15:03:57 +0530

    Time: 688 ms (24.13%), Space: 17.5 MB (65.00%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -6,10 +6,8 @@ public:
         int res=0;
         for(int i=1;i<=n;i++)
         {
-            // dp[i-1][0]=0;
             for(int j=1;j<=m;j++)
             {
-                // dp[0][j-1]=0;
                 if(nums1[i-1]==nums2[j-1])
                     dp[i][j]=dp[i-1][j-1]+1;
                 else
