efc6a37ef6c356ac76b94462ccd3f62b1f72e1b7
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2023-01-31 19:56:13 +0530

    Time: 197 ms (69.80%), Space: 19.2 MB (88.31%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
@@ -1,23 +1,23 @@
 class Solution {
     vector<pair<int,int>>a;
-    vector<vector<int>>dp;
     int cal(int i,int total,int last){
-        if(i>=a.size())
-            return 0;
-        int &ans=dp[i][last+1];
-        if(ans!=-1)
-            return ans;
-        if(last==-1 || a[last].second<=a[i].second){
-            ans=cal(i+1,total+a[i].second,i)+a[i].second;
+        int n=a.size();
+        vector<int>dp(n);
+        int ans=0;
+        for(int i=0;i<n;i++){
+            dp[i]=a[i].second;
+            for(int j=i-1;j>=0;j--){
+                if(a[i].second>=a[j].second)
+                    dp[i]=max(dp[i],dp[j]+a[i].second);
+            }
+            ans=max(ans,dp[i]);
         }
-        ans=max(ans,cal(i+1,total,last));
         return ans;
     }
 public:
     int bestTeamScore(vector<int>& scores, vector<int>& ages) {
         int n=ages.size();
         a.resize(n);
-        dp.resize(n,vector<int>(n+2,-1));
         for(int i=0;i<n;i++){
             a[i].first=ages[i];
             a[i].second=scores[i];
