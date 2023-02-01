62d3f12b2fd4d5dc8981f9ba560531d3d55f4087
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2023-02-01 11:12:14 +0530

    Time: 67 ms (5.36%), Space: 94.2 MB (5.18%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -8,6 +8,8 @@ public:
     string gcdOfStrings(string str1, string str2) {
         string temp="";
         string res="";
+        if(str1.length()>str2.length())
+            swap(str1,str2);
         for(int i=0;i<str1.size();i++){
             temp.push_back(str1[i]);
             if(check(temp,str1)and check(temp,str2))
