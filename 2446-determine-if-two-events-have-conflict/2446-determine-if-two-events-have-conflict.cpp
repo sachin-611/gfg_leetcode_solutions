46291c7b75ac4fa1ad312a3556243cca8e48b99b
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2023-02-01 16:01:17 +0530

    Time: 0 ms (100.00%), Space: 11.6 MB (47.09%) - LeetHub

--- 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.cpp
+++ 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.cpp
@@ -1,5 +1,5 @@
 class Solution {
-    bool comp(string s1,string s2){
+    bool comp(string &s1,string &s2){
         int hh1=(s1[0]-'0')*10+(s1[1]-'0');
         int mm1=(s1[3]-'0')*10+(s1[4]-'0');
         int hh2=(s2[0]-'0')*10+(s2[1]-'0');
@@ -10,7 +10,6 @@ class Solution {
     }
 public:
     bool haveConflict(vector<string>& event1, vector<string>& event2) {
-        string s1=event1[0],e1=event1[1],s2=event2[0],e2=event2[1];
         return (((comp(event1[0],event2[0]) and comp(event2[1],event1[0]))or((comp(event2[0],event1[0]) and comp(event1[1],event2[0])))));
     }
 };
