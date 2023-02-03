897c212006b89e7a8a6e9c02dbd64ca018c04551
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2023-02-03 11:03:35 +0530

    Time: 45 ms (26.65%), Space: 16.7 MB (20.04%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if(numRows==1)
+            return s;
+        vector<vector<char>>ok(numRows);
+        int i=0;
+        int next=0;
+        int n=s.length();
+        bool forw=1;
+        while(i<n){
+            ok[next].push_back(s[i]);
+            i++;
+            if(forw){
+                if(next==numRows-1){
+                    next--;
+                    forw=!forw;
+                }else{
+                    next++;
+                }
+            }else{
+                if(next==0){
+                    next=1;
+                    forw=1;
+                }else{
+                    next--;
+                }
+            }
+        }
+        string res="";
+        for(auto i:ok){
+            for(auto j:i)
+                res.push_back(j);
+        }
+        return res;
+    }
+};
