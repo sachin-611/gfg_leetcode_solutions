e3398349004e23bf3f0a0ec8c312f4240404301e
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2023-02-03 13:06:25 +0530

    Time: 60 ms (80.21%), Space: 28.6 MB (91.98%) - LeetHub

--- 1946-largest-number-after-mutating-substring/1946-largest-number-after-mutating-substring.cpp
+++ 1946-largest-number-after-mutating-substring/1946-largest-number-after-mutating-substring.cpp
@@ -1,19 +1,16 @@
 class Solution {
-    bool pos(const string &num,int i,const vector<int>&change,int prev){
-        int digi=num[i]-'0';
-        if(digi<change[digi] or (prev and digi==change[digi]))
-            return 1;
-        return 0;
-    }
 public:
     string maximumNumber(string &num, const vector<int>& change) {
         int i=0,n=num.size();
         while(i<n){
             int chan=0;
-            while(i<n and pos(num,i,change,chan)){
+            int digi=num[i]-'0';
+            while(i<n and (digi<change[digi] or (chan and digi==change[digi]))){
                 chan=1;
                 num[i]=change[num[i]-'0']+'0';
                 i++;
+                if(i<n)
+                digi=num[i]-'0';
             }
             if(chan)
                 break;
