16897d348e4d9ac2af2132a9910a802cb1dc8064
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2023-02-07 13:05:48 +0530

    Time: 34 ms (27.37%), Space: 8.6 MB (91.91%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     vector<int> findAnagrams(string &s, string &p) {
-        int m=s.size();
-        int n=p.size();
-        vector<int>cur(26),tar(26);
-        vector<int>res;
+        int m=s.size(),n=p.size(),left=0,right=0,flag;
+        vector<int>cur(26),tar(26),res;
         for(auto i:p){
             tar[i-'a']++;
         }
-        int left=0,right=0;
         for(int i=0;i<n and i<m;i++){
             cur[s[right]-'a']++;
             right++;
         }
         for(int i=n;i<=m;i++){
-            int flag=1;
+            flag=1;
             for(int tem=0;tem<26;tem++){
                 if(cur[tem]!=tar[tem])
                     flag=0;
