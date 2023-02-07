d21e5ac9531368c768cf241733e0d2c2eabedd3a
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2023-02-07 15:42:44 +0530

    Time: 4 ms (95.79%), Space: 7.1 MB (98.08%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool isAnagram(string s, string t) {
+    bool isAnagram(string &s, string &t) {
         vector<int>m(26);
         for(auto &i:s){
             m[i-'a']++;
