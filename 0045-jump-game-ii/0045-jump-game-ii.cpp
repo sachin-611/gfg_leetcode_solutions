810539fa72bc4134268dd84c545cec8c706c8830
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2023-02-08 20:22:41 +0530

    Time: 138 ms (43.69%), Space: 17.2 MB (39.26%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n=nums.size();
+        if(n<=1)
+            return 0;
+        vector<int>cnts(n);
+        cnts[0]=0;
+        for(int i=0;i<n;i++){
+            int &jmp=cnts[i];
+            if(cnts[i]==0 and i)
+                continue;
+            for(int j=i+nums[i];j>i;j--){
+                if(j>=n)
+                {
+                    if(cnts[n-1]==0 or(cnts[n-1]>jmp+1))
+                    cnts[n-1]=jmp+1;
+                    continue;
+                }
+                if(cnts[j]!=0)
+                    continue;
+                cnts[j]=jmp+1;
+                // cout<<j<<" "<<cnts[j]<<endl;
+            }
+        }
+        return cnts[n-1];
+    }
+};
