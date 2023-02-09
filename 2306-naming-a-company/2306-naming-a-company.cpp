4b861b7032c09f8127f82a767ae39e1510191cf9
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2023-02-10 01:59:11 +0530

    Time: 906 ms (44.72%), Space: 83.3 MB (56.91%) - LeetHub

---
+++ 2306-naming-a-company/2306-naming-a-company.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    long long distinctNames(vector<string>& ideas) {
+        long long res=0;
+        int n=ideas.size();
+        unordered_set<string>ne[26];
+        for(int i=0;i<26;i++){
+            ne[i].clear();
+        }
+        for(string &str:ideas){
+            ne[str[0]-'a'].insert(str.substr(1));
+        }
+        for(int i=0;i<26;i++){
+            for(int j=0;j<26;j++){
+                int cnt=0;
+                for(string str:ne[i]){
+                    if(ne[j].count(str))
+                        cnt++;
+                }
+                res=res+(ne[i].size()-cnt)*(ne[j].size()-cnt);
+            }
+        }
+        return res;
+    }
+};
