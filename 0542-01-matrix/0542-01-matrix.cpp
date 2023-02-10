dd9a58069c24a4b6aa8bc3ce8ef313ef64cf7aff
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2023-02-10 16:29:36 +0530

    Time: 70 ms (94.87%), Space: 30 MB (78.51%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        vector<vector<int>>res(mat);
+        int n=mat.size(),m=mat[0].size();
+        queue<pair<int,int>>q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0)
+                    q.push({i,j});
+            }
+        }
+        int level=0,sz;
+        pair<int,int>ok;
+        while(q.empty()==false){
+            sz=q.size();
+            level++;
+            for(int i=0;i<sz;i++){
+                ok=q.front();
+                if(ok.first-1>=0 and mat[ok.first-1][ok.second]==1){
+                    mat[ok.first-1][ok.second]=0;
+                    res[ok.first-1][ok.second]=level;
+                    q.push({ok.first-1,ok.second});
+                }
+                if(ok.second-1>=0 and mat[ok.first][ok.second-1]==1){
+                    mat[ok.first][ok.second-1]=0;
+                    res[ok.first][ok.second-1]=level;
+                    q.push({ok.first,ok.second-1});
+                }
+                if(ok.first+1<n and mat[ok.first+1][ok.second]==1){
+                    mat[ok.first+1][ok.second]=0;
+                    res[ok.first+1][ok.second]=level;
+                    q.push({ok.first+1,ok.second});
+                }
+                if(ok.second+1<m and mat[ok.first][ok.second+1]==1){
+                    mat[ok.first][ok.second+1]=0;
+                    res[ok.first][ok.second+1]=level;
+                    q.push({ok.first,ok.second+1});
+                }
+                q.pop();
+            }
+        }
+        return res;
+    }
+};
