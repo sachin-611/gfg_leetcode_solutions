08b085df39537d95557e03b267fe772d6ee50386
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2023-02-10 15:03:16 +0530

    Time: 61 ms (90.38%), Space: 19.8 MB (81.27%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maxDistance(vector<vector<int>>& grid) {
-        queue<vector<int>>q;
+        queue<pair<int,int>>q;
         int n=grid.size();
         int res=-1;
         for(int i=0;i<n;i++){
@@ -11,29 +11,29 @@ public:
                 }
             }
         }
-        if(q.size()==n*n or q.empty()){
-            return -1
-        ;}
+        int sz=q.size();
+        if(sz==n*n or sz==0){return -1;}
         int level=0;
+        pair<int,int>ok;
         while(q.empty()==false){
-            int sz=q.size();
+            sz=q.size();
             for(int ii=0;ii<sz;ii++){
-                vector<int>&ok=q.front();
-                if(ok[0]-1>=0 and grid[ok[0]-1][ok[1]]==0){
-                    grid[ok[0]-1][ok[1]]=1;
-                    q.push({ok[0]-1,ok[1]});
+                    ok=q.front();
+                if(ok.first-1>=0 and grid[ok.first-1][ok.second]==0){
+                    grid[ok.first-1][ok.second]=1;
+                    q.push({ok.first-1,ok.second});
                 }
-                if(ok[1]-1>=0 and grid[ok[0]][ok[1]-1]==0){
-                    grid[ok[0]][ok[1]-1]=1;
-                    q.push({ok[0],ok[1]-1});
+                if(ok.second-1>=0 and grid[ok.first][ok.second-1]==0){
+                    grid[ok.first][ok.second-1]=1;
+                    q.push({ok.first,ok.second-1});
                 }
-                if(ok[0]+1<n and grid[ok[0]+1][ok[1]]==0){
-                    grid[ok[0]+1][ok[1]]=1;
-                    q.push({ok[0]+1,ok[1]});
+                if(ok.first+1<n and grid[ok.first+1][ok.second]==0){
+                    grid[ok.first+1][ok.second]=1;
+                    q.push({ok.first+1,ok.second});
                 }
-                if(ok[1]+1<n and grid[ok[0]][ok[1]+1]==0){
-                    grid[ok[0]][ok[1]+1]=1;
-                    q.push({ok[0],ok[1]+1});
+                if(ok.second+1<n and grid[ok.first][ok.second+1]==0){
+                    grid[ok.first][ok.second+1]=1;
+                    q.push({ok.first,ok.second+1});
                 }
                 q.pop();
             }
