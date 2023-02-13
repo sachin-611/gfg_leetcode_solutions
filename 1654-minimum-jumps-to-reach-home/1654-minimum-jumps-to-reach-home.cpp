6b8c85bc5c2a098459e3a7b3a06b23d87f801d36
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2023-02-13 18:51:00 +0530

    Time: 77 ms (47.50%), Space: 24.1 MB (56.58%) - LeetHub

---
+++ 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minimumJumps(vector<int>& forbidden, int a, int b, int x) {
+        // if(gcd(x,abs(a-b))==1)
+        //     return -1;
+        queue<pair<int,int>>q;
+        pair<int,int>p;
+        q.push({0,1});
+        int level=0,sz;
+        unordered_set<int>cnts(forbidden.begin(),forbidden.end());
+        set<pair<int,int>>vis;
+        vis.insert({0,1});
+        while(q.empty()==false){
+            sz=q.size();
+            while(sz--){
+                p=q.front();
+                q.pop();
+                if(p.first==x)
+                    return level;
+                if(p.second==1 and p.first-b>=0 and cnts.count(p.first-b)==0 and vis.count({p.first-b,0})==0){
+                    q.push({p.first-b,0});
+                    vis.insert({p.first-b,0});
+                }
+                if(p.first+a<5998 and cnts.count(p.first+a)==0 and vis.count({p.first+a,1})==0){
+                    q.push({p.first+a,1});
+                    vis.insert({p.first+a,1});
+                }
+            }
+            level++;
+        }
+        return -1;
+    }
+};
