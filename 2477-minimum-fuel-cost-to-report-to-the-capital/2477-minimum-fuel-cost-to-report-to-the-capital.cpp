6a44671ac56a8d21fb98bde9efee9961e415a5ef
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2023-02-13 13:40:03 +0530

    Time: 793 ms (35.01%), Space: 227.7 MB (16.15%) - LeetHub

--- 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.cpp
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.cpp
@@ -1,31 +1,31 @@
 class Solution {
+        unordered_map<int,vector<int>>m;
+    long long ans;
+    int seat;
+    vector<int>vis;
 public:
-//     long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
-//         unordered_map<int,vector<int>>m;
-//         unordered_set<int>cnt;
-//         for( auto &i:roads){
-//             m[i[0]].push_back(i[1]);
-//             m[i[1]].push_back(i[0]);
-//         }
-        
-//         return 0;
-//     }
-    long long ans = 0; int s;
-long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
-    vector<vector<int>> graph(roads.size() + 1); s = seats;
-    for (vector<int>& r: roads) {
-        graph[r[0]].push_back(r[1]);
-        graph[r[1]].push_back(r[0]);
+    long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
+        unordered_set<int>cnt;
+        for( auto &i:roads){
+            m[i[0]].push_back(i[1]);
+            m[i[1]].push_back(i[0]);
+        }
+        ans=0;
+        seat=seats;
+        vis.resize(roads.size()+1);
+        dfs(0);
+        return ans;
     }
-    dfs(0, 0, graph);
-    return ans;
-}
-int dfs(int i, int prev, vector<vector<int>>& graph, int people = 1) {
-    for (int& x: graph[i]) {
-        if (x == prev) continue;
-        people += dfs(x, i, graph);
+    long long dfs(int node){
+        vis[node]=1;
+        long long res=1;
+        for(auto child:m[node]){
+            if(vis[child]==0){
+                long long temp=dfs(child);
+                ans+=(temp+seat-1)/seat;  
+                res+=temp;
+            }
+        }
+        return res;
     }
-    if (i != 0) ans += (people + s - 1) / s;
-    return people;
-}
 };
