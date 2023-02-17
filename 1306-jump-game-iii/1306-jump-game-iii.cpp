06ef92d4b9629324d1e092e4a2e99b0c0d4fa908
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2023-02-18 00:50:56 +0530

    Time: 61 ms (29.17%), Space: 32.5 MB (64.98%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -3,7 +3,8 @@ public:
     bool canReach(vector<int>& arr, int start) {
         queue<int>q;
         q.push(start);
-        vector<int>vis(arr.size(),0);
+        const int sz=arr.size();
+        vector<int>vis(sz,0);
         while(q.empty()==false){
             auto front=q.front();
             q.pop();
@@ -13,7 +14,7 @@ public:
             if(front-arr[front]>=0 and vis[front-arr[front]]==0){
                 q.push(front-arr[front]);
             }
-            if(front+arr[front]<arr.size() and vis[front+arr[front]]==0){
+            if(front+arr[front]<sz and vis[front+arr[front]]==0){
                 q.push(front+arr[front]);
             }
         }
