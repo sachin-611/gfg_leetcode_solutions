02bce8fb2aa66449394910102847cc7afc7bc598
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2023-02-21 12:11:49 +0530

    Time: 32 ms (43.92%), Space: 22.2 MB (93.44%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,5 +1,5 @@
 class Solution {
-    int isodd(vector<int>&nums,int ind,int prev){
+    int isodd(vector<int>&nums,int &ind,int prev){
         if(nums[ind]!=nums[ind-1] and nums[ind]!=nums[ind+1]){
             return 69;
         }
@@ -23,19 +23,24 @@ class Solution {
     }
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        if(nums.size()==1)
+        if(nums.size()==1){
             return nums[0];
-        if(nums[0]!=nums[1])
+        }
+        if(nums[0]!=nums[1]){
             return nums[0];
-        if(nums[nums.size()-1]!=nums[nums.size()-2])
+        }
+        if(nums[nums.size()-1]!=nums[nums.size()-2]){
             return nums[nums.size()-1];
+        }
         
         int low=0,high=nums.size()-1;
         while(low<=high){
             int mid=(low+high)/2;
             int val=isodd(nums,mid,mid-low);
-            if(val==69)
+                
+            if(val==69){
                 return nums[mid];
+            }
             if(val<0){
                 high=mid+val;
             }
