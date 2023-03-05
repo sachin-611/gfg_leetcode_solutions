622c210be1de4fe199193f6a674cd385a083a922
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2023-03-05 09:49:49 +0530

    Time: 358 ms (100.00%), Space: 15.4 MB (33.33%) - LeetHub

---
+++ 2585-number-of-ways-to-earn-points/2585-number-of-ways-to-earn-points.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>>dp;
+    int mod=1e9+7;
+    long long solve(int i,int n,vector<vector<int>>&a,int target){
+        if(target==0){
+            return 1;
+        }
+        if(i==n){
+            return target==0;
+        }
+        if(dp[i][target]!=-1){
+            return dp[i][target]%mod;
+        }
+        long long res=0;
+        for(int j=0;j<=a[i][0];j++){
+            if(target-a[i][1]*j>=0){
+                res+=solve(i+1,n,a,target-a[i][1]*j);
+            }
+            else
+                break;
+        }
+        return dp[i][target]=res%mod;
+    }
+    long long waysToReachTarget(int target, vector<vector<int>>& types) {
+        dp.resize(types.size()+5,vector<int>(target+2,-1));
+        return solve(0,types.size(),types,target);
+    }
+};
