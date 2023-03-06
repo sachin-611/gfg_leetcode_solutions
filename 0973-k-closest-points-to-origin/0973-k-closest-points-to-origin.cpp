428eb6d1adeef4c632464b24934a4a0f2a32fbb8
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2023-03-06 14:29:07 +0530

    Time: 282 ms (51.27%), Space: 60.4 MB (87.59%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -4,29 +4,18 @@ int get_distance(const vector<int>&a){
 class point{
     public:
     bool operator()(const vector<int>&a,const vector<int>&b){
-        return get_distance(a)<get_distance(b);
+        return get_distance(a)>get_distance(b);
     }
 };
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue<vector<int>,vector<vector<int>>,point>p;
-        for(auto &i:points){
-            if(p.size()<k){
-                p.push(i);
-            }else{
-                int cur=get_distance(i);
-                int tops=get_distance(p.top());
-                if(cur<tops){
-                    p.pop();
-                    p.push(i);
-                }
-            }
-        }
         vector<vector<int>>res;
-        while(p.empty()==false){
-            res.push_back(p.top());
-            p.pop();
+        make_heap(points.begin(),points.end(),point());
+        for(int i=0;i<k;i++){
+            res.push_back(points[0]);
+            pop_heap(points.begin(),points.end(),point());
+            points.pop_back();
         }
         return res;
     }
