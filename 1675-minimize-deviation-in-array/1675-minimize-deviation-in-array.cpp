76dcf2949f8c13e8deb1e5a3e1073e42713c3c9d
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2023-03-06 10:28:34 +0530

    Time: 564 ms (61.76%), Space: 56.8 MB (52.30%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int minimumDeviation(vector<int>& nums) {
         priority_queue<int>pq;
-        int mini=nums[0]*2,diff=1e9+5,ans=-1;
+        int mini=nums[0]*2,diff=INT_MAX,ans=-1;
         for(auto i:nums){
             if(i%2){
                 i*=2;
