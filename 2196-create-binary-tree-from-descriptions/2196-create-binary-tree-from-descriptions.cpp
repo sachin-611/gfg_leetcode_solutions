64eb5f3d873c86836fa2cc66b014de35d74b4da0
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2023-03-11 15:41:22 +0530

    Time: 972 ms (92.56%), Space: 277.8 MB (80.67%) - LeetHub

--- 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
@@ -14,19 +14,26 @@ public:
     TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
         unordered_set<int>ok;
         unordered_map<int,TreeNode*>m;
+        TreeNode* par,*child;
         for(vector<int>&i:descriptions){
             if(!m.count(i[0])){
-                m[i[0]]=new TreeNode(i[0]);
+                par=new TreeNode(i[0]);
+                m[i[0]]=par;
+            }else{
+                par=m[i[0]];
             }
             if(!m.count(i[1])){
-                m[i[1]]=new TreeNode(i[1]);   
+                child=new TreeNode(i[1]);   
+                m[i[1]]=child;
+            }else{
+                child=m[i[1]];
             }
             if(i[2]){
-                m[i[0]]->left=m[i[1]];
+                par->left=child;
             }else{
-                m[i[0]]->right=m[i[1]];
+                par->right=child;
             }
-            ok.insert(i[1]);
+            ok.insert(child->val);
         }
         for(vector<int>&i:descriptions){
             if(!ok.count(i[0])){
