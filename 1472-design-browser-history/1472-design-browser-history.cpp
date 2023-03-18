58f21f0f885b47e907e79147a319b056473f9947
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2023-03-18 09:26:44 +0530

    Time: 153 ms (44.50%), Space: 60.1 MB (65.15%) - LeetHub

---
+++ 1472-design-browser-history/1472-design-browser-history.cpp
@@ -0,0 +1,49 @@
+struct node{
+  node *next,*prev;
+    string data;
+};
+class BrowserHistory {
+public:
+    node* cur;
+    BrowserHistory(string homepage) {
+        cur= new node;
+        cur->data=homepage;
+        cur->next=cur->prev=nullptr;
+    }
+    
+    void visit(string url) {
+        cur->next=new node();
+        cur->next->prev=cur;
+        cur=cur->next;
+        cur->next=nullptr;
+        cur->data=url;
+    }
+    
+    string back(int steps) {
+        while(steps){
+            steps--;
+            if(cur->prev){
+                cur=cur->prev;
+            }
+        }
+        return cur->data;
+    }
+    
+    string forward(int steps) {
+        while(steps){
+            steps--;
+            if(cur->next){
+                cur=cur->next;
+            }
+        }
+        return cur->data;
+    }
+};
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory* obj = new BrowserHistory(homepage);
+ * obj->visit(url);
+ * string param_2 = obj->back(steps);
+ * string param_3 = obj->forward(steps);
+ */
