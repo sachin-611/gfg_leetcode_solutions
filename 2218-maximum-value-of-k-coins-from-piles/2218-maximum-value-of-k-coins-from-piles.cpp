6ad78c93494fa450b561ec9129fe4d05d10bf4e1
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2023-04-16 00:13:15 +0530

    Time: 272 ms (73.56%), Space: 19.8 MB (12.83%) - LeetHub

---
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int dp[1001][2001];
+    int maxValueOfCoins(vector<vector<int>>& piles, int k,int i=0) {
+        if(i==piles.size()){
+            return 0;
+        }
+        if(i==0){
+            memset(dp,-1,sizeof(dp));
+        }
+        if(k==0){
+            return 0;
+        }
+        int &res=dp[i][k];
+        if(res!=-1){
+            return res;
+        }
+        int cnt=0;
+        int total=0;
+        res=maxValueOfCoins(piles,k,i+1);
+        for(int &j:piles[i]){
+            cnt++;
+            total+=j;
+            if(cnt<=k){
+                res=max(res,maxValueOfCoins(piles,k-cnt,i+1)+total);
+            }else{
+                break;
+            }
+        }
+        return res;
+    }
+};
