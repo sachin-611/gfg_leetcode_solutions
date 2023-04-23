a2f7bdaf0466cb2dcfee3e5d304138dbdfaa8992
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2023-04-24 02:46:36 +0530

    Time: 98 ms (26.47%), Space: 36.8 MB (11.77%) - LeetHub

---
+++ 1416-restore-the-array/1416-restore-the-array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int>dp;
+    int mod=1000000007;
+    int numberOfArrays(string &s, int k,int i=0) {
+        if(i==0){
+            dp.resize(s.size(),-1);
+        }
+        if(i==s.size())
+            return 1;
+        int &res=dp[i];
+        if(res!=-1)
+            return res;
+        res=0;
+        long long nums=0;
+        for(int j=i;j<s.size();j++){
+            nums=nums*10+(s[j]-'0');
+            if(nums==0)
+                return 0;
+            if(nums>k)
+                break;
+            res=(res+numberOfArrays(s,k,j+1))%mod;
+        }
+        return res%=mod;
+    }
+};
