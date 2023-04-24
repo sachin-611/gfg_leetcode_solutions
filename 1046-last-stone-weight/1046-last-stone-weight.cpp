a66966323d34d7fa877403d0de252d73c62ce1b2
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2023-04-24 10:31:42 +0530

    Time: 2 ms (49.25%), Space: 7.7 MB (41.71%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        while(stones.size()>1){
+            sort(stones.begin(),stones.end());
+            int y=stones.back();
+            stones.pop_back();
+            int x=stones.back();
+            stones.pop_back();
+            if(x==y){
+                continue;
+            }else{
+                stones.push_back(y-x);
+            }
+        }
+        if(stones.empty())
+            return 0;
+        return stones[0];
+    }
+};
