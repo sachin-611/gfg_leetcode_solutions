ce1e37299cb6e18aeb13c915905c2c56562142aa
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2023-04-28 13:00:09 +0530

    Time: 1068 ms (5.07%), Space: 421.9 MB (5.40%) - LeetHub

--- 0839-similar-string-groups/0839-similar-string-groups.cpp
+++ 0839-similar-string-groups/0839-similar-string-groups.cpp
@@ -28,7 +28,7 @@ public:
         ok.resize(n);
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                if(i!=j and pos(strs[i],strs[j])){
+                if(pos(strs[i],strs[j])){
                     g[i].push_back(j);
                     g[j].push_back(i);
                 }
