07f7fd8e43a2e0ab8fe0cbc10ab412e5f2d01e9e
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2023-04-28 14:25:45 +0530

    Time: 98 ms (8.63%), Space: 6.3 MB (17.99%) - LeetHub

---
+++ 0672-bulb-switcher-ii/0672-bulb-switcher-ii.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    set<int>ok;
+        int bit;
+    int ans;
+    int type1(int n){
+        for(int i=1;i<=bit;i++){
+            n=n^((1<<(i-1)));
+        }
+        return n;
+    }
+    int type2(int n){
+        for(int i=1;i<=bit;i++){
+            if(i%2==0)
+            n=n^((1<<(i-1)));
+        }
+        return n;
+    }
+    int type3(int n){
+        for(int i=1;i<=bit;i++){
+            if(i%2==1)
+            n=n^((1<<(i-1)));
+        }
+        return n;
+    }
+    int type4(int n){
+        for(int i=1;i<=bit;i++){
+            if(i%3==1)
+            n=n^((1<<(i-1)));
+        }
+        return n;
+    }
+    void solve(int n,int presses){
+        if(presses==0){
+            ok.insert(n);
+            ans=max(ans,(int)ok.size());
+            return;
+        }
+        solve(type1(n),presses-1);
+        
+        solve(type2(n),presses-1);
+        
+        solve(type3(n),presses-1);
+        
+        solve(type4(n),presses-1);
+    }
+    void print(int o){
+        if(o==0){
+            cout<<"0";
+        }
+        while(o){
+            cout<<(o&1)<<" ";
+            o/=2;
+        }
+        cout<<endl;
+    }
+    int flipLights(int n, int presses) {
+        int num=0;
+        bit=min(n,30);
+        presses=min(8,presses);
+        for(int i=1;i<=bit;i++){
+            num=num|(1<<(i-1));
+        }
+        ans=0;
+        solve(num,presses);
+        return ans;
+    }
+};
