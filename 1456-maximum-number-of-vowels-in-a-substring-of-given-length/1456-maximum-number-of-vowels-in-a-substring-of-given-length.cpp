a6321c602f86aee50fc2351ffc0bd2f70eb941c6
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2023-05-05 20:05:19 +0530

    Time: 19 ms (81.96%), Space: 9.1 MB (100.00%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -3,7 +3,7 @@ public:
     inline bool check(char &s){
         return (s=='a' || s=='e' || s=='i' || s=='o' || s=='u');
     }
-    int maxVowels(string &s, int k) {
+    int maxVowels(string &s, int &k) {
         int res=0,cur=0,n=s.length();
         for(int i=0;i<n;i++){
             if(check(s[i]))
