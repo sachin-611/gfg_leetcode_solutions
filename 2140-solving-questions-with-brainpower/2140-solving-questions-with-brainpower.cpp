f1cccf29713483a87594577cb2c5af1e2c9911fb
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2023-05-13 16:12:38 +0530

    Time: 396 ms (29.82%), Space: 121.5 MB (50.22%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    long long dp[100005];
-    long long res(int i,vector<vector<int>>&q,int n){
+    vector<long long>dp;
+        int n;
+    long long res(int i,vector<vector<int>>&q){
         if(i>=n)
             return 0;
         if(dp[i]!=-1)
             return dp[i];
-        long long p1=res(i+1,q,n);
-        long long p2=res(i+q[i][1]+1,q,n)+q[i][0];
-        return dp[i]=max(p1,p2);
+        return dp[i]=max(res(i+1,q),res(i+q[i][1]+1,q)+q[i][0]);
     }
     long long mostPoints(vector<vector<int>>& questions) {
-        memset(dp,-1,sizeof(dp));
-        return res(0,questions,questions.size());
+        n=questions.size();
+        dp.resize(n,-1);
+        return res(0,questions);
     }
 };
