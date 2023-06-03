6757a5c309d1b0b5fac6c86bebe3e8b81b4b2e29
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2023-06-03 12:59:26 +0530

    Time: 209 ms (60.29%), Space: 73.6 MB (55.87%) - LeetHub

---
+++ 2649-nested-array-generator/2649-nested-array-generator.js
@@ -0,0 +1,25 @@
+/**
+ * @param {Array} arr
+ * @return {Generator}
+ */
+/**
+ * @param {Array} arr
+ * @return {Generator}
+ */
+var inorderTraversal = function* (arr) {
+    if(!Array.isArray(arr)) {
+        yield arr
+        return
+    }
+
+    for(let i = 0; i < arr.length; i++) {
+        yield* inorderTraversal(arr[i])
+    }
+};
+
+/**
+ * const gen = inorderTraversal([1, [2, 3]]);
+ * gen.next().value; // 1
+ * gen.next().value; // 2
+ * gen.next().value; // 3
+ */
