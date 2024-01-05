081c0a197ddcf5c19aad4e9ae3650122ab983449
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2024-01-05 09:09:07 +0530

    Time: 142 ms (20.97%), Space: 85.4 MB (30.43%) - LeetHub

---
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        map<int,int>m;
+        for(int i:nums){
+            m[i]++;
+        }
+        int ans=0;
+        for(auto i:m){
+            if(i.second==1)
+                return -1;
+            int cnt=ans;
+            if(i.second%3==0 or i.second%3==2){
+                ans+=i.second/3;
+                ans+=(i.second%3>0?1:0);
+            }else{
+                ans+=i.second/3-1;
+                ans+=2;
+            }
+            // cout<<i.first<<" "<<i.second<< " "<<ans-cnt<<endl;;
+        }
+        return ans;
+    }
+};
