d78aee89418db6dca7bd5d7482f67207b80ee21c
Sachin Choudhary <54989437+sachin-611@users.noreply.github.com>
2024-01-17 18:50:51 +0530

    Time: 5 ms (39.16%), Space: 8.8 MB (20.45%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -3,8 +3,8 @@ public:
     bool uniqueOccurrences(vector<int>& arr) {
         unordered_map<int,int>m;
         unordered_set<int>s;
-        for(int i:arr)m[i]++;
-        for(auto i:m){
+        for(int &i:arr)m[i]++;
+        for(auto &i:m){
             s.insert(i.second);
         }
         return m.size()==s.size();
